09c36c768704dcd792d205e53aaee1796d28baaf
leonardofj <leonardofj@gmail.com>
2024-04-17 01:09:06 +0200

    feat: vscode installer

---
+++ scripts/programs/vsc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# https://code.visualstudio.com/docs/setup/linux
+echo "⌨️  Installing VSCode"
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install -y code
+rm microsoft.gpg
+
+# installing extensions
+function install {
+  name="${1}"
+  code --install-extension ${name} --force
+}
+
+mapfile -t extensions <lists/vscode_extensions.txt
+
+for extension in "${extensions[@]}"; do
+  install "$extension"
+done
